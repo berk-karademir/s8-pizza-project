060bc0c42c993e2ff0b259deb33d014d2c283dbe
berk-karademir <berk.karademir@hotmail.com>
2024-11-05 15:53:35 +0300

    all forms added
    forms done til 'quantity counter'

--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-        
+//https://reactstrap.github.io/?path=/docs/components-forms--input
 export default function Order() {
    return (
+    <>
    <div>
-        <h1> Teknolojik Yemekler</h1>
-        <span>Anasayfa &gt; Sipariş Oluştur</span>
+    <h1> Teknolojik Yemekler</h1>
+    <span>Anasayfa &gt; Sipariş Oluştur</span>
+  </div>
         <main>
           <h2>Position Absolute Acı Pizza</h2>
            <p>89.90 <span>4.9</span></p>
            <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.</p>
-           <Form>
-           <FormGroup>
+           
+           
+  <Form>
+    <FormGroup>
+      {/*------------------   SIZE SELECT       ------------------------*/ }
       <Label  htmlFor="sizeSelect">Select size</Label>
      
         <Input
           type="radio"
-          name="small"
+          name="sizeSelect"
           id="small"
           value="small"
           checked={""}
@@ -27,7 +32,7 @@ export default function Order() {
       
         <Input
           type="radio"
-          name="medium"
+          name="sizeSelect"
           id="medium"
           value="medium"
           checked={""}
@@ -38,16 +43,28 @@ export default function Order() {
 
         <Input
           type="radio"
-          name="large"
+          name="sizeSelect"
           id="large"
           value="large"
           checked={""}
           onChange={""}
         />
         <Label htmlFor="large">Large</Label>
-      
+
+
+        <Input
+          type="radio"
+          name="sizeSelect"
+          id="small"
+          value="small"
+          checked={""}
+          onChange={""}
+        />
+        <Label htmlFor="small" >Small</Label>
     </FormGroup>
 
+
+{/*------------------     DOUGH SELECT       ------------------------*/ }
     <FormGroup>
         <Label htmlFor="doughSelect">Select dough</Label>
         <Input type="select" name="dough" id="dough"  value={""} onChange={""}>
@@ -59,6 +76,7 @@ export default function Order() {
         </Input>
       </FormGroup>
 
+{/*------------------     CRUST SELECT       ------------------------*/ }
       <FormGroup>
         <Label htmlFor="crustSelect">Select crust</Label>
         <Input type="select" name="crust" id="crust"  value={""} onChange={""}>
@@ -73,15 +91,198 @@ export default function Order() {
       </FormGroup>
 
 
-      <FormGroup>
-        <input type="checkbox" />
+{/*------------------     TOPPINGS SELECT       ------------------------*/ }
+      <FormGroup >
+      Toppings
+      <span>Maximum 10 toppings. 5₺</span>
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"pepperoni"}
+        onChange={""}
+        checked={""}
+        />
+        Pepperoni
+        </Label>
 
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"BBQChicken"}
+        onChange={""}
+        checked={""}
+        />
+        BBQ Chicken
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"corn"}
+        onChange={""}
+        checked={""}
+        />
+        Corn
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"garlic"}
+        onChange={""}
+        checked={""}
+        />
+        Garlic
+        </Label>
+
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"pineapple"}
+        onChange={""}
+        checked={""}
+        />
+        Pineapple
+        </Label>
+        
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"sausage"}
+        onChange={""}
+        checked={""}
+        />
+        Sausage
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"onion"}
+        onChange={""}
+        checked={""}
+        />
+        Onion
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"fermentedSausage"}
+        onChange={""}
+        checked={""}
+        />
+        Fermented Sausage
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"sucuk"}
+        onChange={""}
+        checked={""}
+        />
+        Organic Turkish Sausage
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"wurst"}
+        onChange={""}
+        checked={""}
+        />
+        Organic German Wurst
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"charleston"}
+        onChange={""}
+        checked={""}
+        />
+        Charleston Pepper
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"zucchini"}
+        onChange={""}
+        checked={""}
+        />
+        Zucchini
+        </Label>
+
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"ham"}
+        onChange={""}
+        checked={""}
+        />
+        Canadian Ham
+        </Label>
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"tomato"}
+        onChange={""}
+        checked={""}
+        />
+        Tomato
+        </Label>
+
+
+        <Label htmlFor='toppingSelect'>
+        <Input
+        type='checkbox'
+        name='toppingSelect'
+        value={"jalapeno"}
+        onChange={""}
+        checked={""}
+        />
+        Jalapeño
+        </Label>
 
       </FormGroup>
-    </Form>
+{/*------------------    ORDER NOTES      ------------------------*/ }
+      <FormGroup>
+    <Label htmlFor="orderNotes">
+      Order Notes
+    <Input
+      name="orderNotes"
+      type="textarea"
+      placeholder='Would you like to add any notes to your order?'
+      value=""
+      onChange={""}
+    />
+     </Label>
+  </FormGroup>
+</Form>
         
 
            
         </main>
-      </div>)
+      
+      </>
+      )
 }
