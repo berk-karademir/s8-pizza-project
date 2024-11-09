3cc4516bfd01ef8eb26491808da4f8596101d1b0
berk-karademir <berk.karademir@hotmail.com>
2024-11-09 15:26:57 +0300

    footer anchor tag hrefs' paths fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,18 @@
 export default function Footer() {
-    return(
-
-        <footer>
-        <div className='footer-container'>
-          <span>
-          <img src="./src/assets/visa.svg" alt="visa card"/>
-          <img src="./src/assets/master-card.svg" alt="master card"/>
-          <img src="./src/assets/american-express.svg" alt="american express"/>
-          </span>
-          <a href="./src/workInProgress.html" target="_blank">© 2024 Teknolojik Yemekler, Inc.</a>
-          <a href="src\workInProgress.html" target="_blank">Terms & Policy</a>
-          <a href="src\workInProgress.html" target="_blank">Contact & Support</a>
-          <a href="src\workInProgress.html" target="_blank">Manage cookies</a>
-          <a href="src\workInProgress.html" target="_blank">Do not share my personal info</a>
-        </div>
+  return (
+      <footer>
+          <div className='footer-container'>
+              <span>
+                  <img src="./src/assets/visa.svg" alt="visa card" />
+                  <img src="./src/assets/master-card.svg" alt="master card" />
+                  <img src="./src/assets/american-express.svg" alt="american express" />
+              </span>
+              <a href="./workInProgress.html" target="_blank">© 2024 Teknolojik Yemekler, Inc.</a>
+              <a href="./workInProgress.html" target="_blank">Terms & Policy</a>
+              <a href="./workInProgress.html" target="_blank">Contact & Support</a>
+              <a href="./workInProgress.html" target="_blank">Manage cookies</a>
+              <a href="./workInProgress.html" target="_blank">Do not share my personal info</a>
+          </div>
       </footer>
-      
-    )
+  )
 }
