2dbef3bb242601fbd3ebd8aaed8ab651a51f80f3
berk-karademir <berk.karademir@hotmail.com>
2024-11-09 12:58:52 +0300

    script added to package.json
    npm run start command is now launches both local host and cypress app

---
+++ cypress/e2e/s8-pizza-project.cy.js
@@ -0,0 +1,11 @@
+// app.cy.js
+
+describe('Home Page Tests', () => {
+  
+  it('should load the home page correctly', () => {
+    cy.visit("http://localhost:5173");// Ana sayfayı belirtilen URL'ye ziyaret et
+
+   
+  });
+  
+});
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ export default function Home({ navOrder }) {
         <div className="home-page"> 
             <h1>TEKNOLOJiK YEMEKLER</h1>
             <p>KOD ACIKTIRIR<br />PİZZA DOYURUR</p>
-            <button onClick={navOrder}> I'M HUNGRY ! </button>
+            <button onClick={navOrder}>I'M HUNGRY</button>
         </div>
     );
 }
