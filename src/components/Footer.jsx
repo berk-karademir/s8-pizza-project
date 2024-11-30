fd9f1a8dcbe89a6f2ec5bfe4e591590cdc563c49
berk-karademir <berk.karademir@hotmail.com>
2024-11-30 16:09:13 +0300

    fixes footer images rendering

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ export default function Footer() {
       <footer>
           <div className='footer-container'>
               <span>
-                  <img src="./src/assets/visa.svg" alt="visa card" />
-                  <img src="./src/assets/master-card.svg" alt="master card" />
-                  <img src="./src/assets/american-express.svg" alt="american express" />
+                  <img src="./visa.svg" alt="visa card" />
+                  <img src="./master-card.svg" alt="master card" />
+                  <img src="./american-express.svg" alt="american express" />
               </span>
               <a href="./workInProgress.html" target="_blank">© 2024 Teknolojik Yemekler, Inc.</a>
               <a href="./workInProgress.html" target="_blank">Terms & Policy</a>
